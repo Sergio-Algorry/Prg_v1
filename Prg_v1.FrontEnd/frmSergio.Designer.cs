2341e8cd64434dc5f71b08649240bbb123f37b9f
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2025-08-12 13:30:15 -0300

    Agregar archivos de proyecto.

---
+++ Prg_v1.FrontEnd/frmSergio.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Prg_v1.FrontEnd
+{
+    partial class frmSergio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.btSaludar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.AutoSize = true;
+            this.lblSaludo.Location = new System.Drawing.Point(202, 124);
+            this.lblSaludo.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(166, 24);
+            this.lblSaludo.TabIndex = 0;
+            this.lblSaludo.Text = "Hola Mundo!!!!!!!";
+            // 
+            // btSaludar
+            // 
+            this.btSaludar.Location = new System.Drawing.Point(378, 52);
+            this.btSaludar.Name = "btSaludar";
+            this.btSaludar.Size = new System.Drawing.Size(123, 29);
+            this.btSaludar.TabIndex = 1;
+            this.btSaludar.Text = "Saludar";
+            this.btSaludar.UseVisualStyleBackColor = true;
+            this.btSaludar.Click += new System.EventHandler(this.btSaludar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(146, 52);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(180, 29);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // frmSergio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 186);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btSaludar);
+            this.Controls.Add(this.lblSaludo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "frmSergio";
+            this.Text = "Primer formulario de Sergio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.Button btSaludar;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
+
---
+++ Prg_v1.FrontEnd/frmSergio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prg_v1.FrontEnd
+{
+    public partial class frmSergio : Form
+    {
+        public frmSergio()
+        {
+            InitializeComponent();
+        }
+
+        private void btSaludar_Click(object sender, EventArgs e)
+        {
+            lblSaludo.Text = "Hola " + txtNombre.Text + "!!!!!!!";
+        }
+    }
+}
